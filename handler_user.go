cb2f4f6a07ba511c39c675c600baf11ea5ac29e5
abooda1990 <alteer888.love@gmail.com>
2024-02-08 01:08:59 +0100

    verion that interacte with database and create user

---
+++ handler_user.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"RSSaggregator/internal/database"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		Name string `josn:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing json : %v", err))
+		return
+	}
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't create user: %v", err))
+		return
+
+	}
+
+	respondWithJSON(w, 200, user)
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"RSSaggregator/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -9,7 +11,12 @@ import (
 
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -18,6 +25,20 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("port is found in the envirment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is found in the envirment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Canot connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -35,6 +56,7 @@ func main() {
 	v1Router.Get("/ready", handlerReadiness)
 
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -44,9 +66,9 @@ func main() {
 	}
 
 	log.Printf("server starting on port %v", portString)
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
